feca6f8d83767821fe34c6ce4a76d5a5499188b6
Dimitar Dimitrov <117978668+DimitarDimitrov007@users.noreply.github.com>
2023-01-18 08:49:04 +0200

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/Numbers from N to 1.js
@@ -0,0 +1,20 @@
+function numbersNToONe(num){
+    while(num >=1){
+        console.log(num);
+        num--;
+    }
+
+}
+numbersNToONe(5);
+numbersNToONe(3);
+
+
+
+function numbersNToONe(num){
+    for(let i = num; i >= 1; i--){
+        console.log(i);
+    }
+
+}
+numbersNToONe(5);
+numbersNToONe(3);
