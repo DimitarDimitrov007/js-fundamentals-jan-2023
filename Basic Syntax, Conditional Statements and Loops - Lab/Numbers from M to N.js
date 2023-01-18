5132403213cfc02a3225508b3057a1942e5d86a0
Dimitar Dimitrov <117978668+DimitarDimitrov007@users.noreply.github.com>
2023-01-18 08:48:51 +0200

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/Numbers from M to N.js
@@ -0,0 +1,19 @@
+function numbersMToN(m,n){
+    for(let i = m; i >= n;i-- ){
+        console.log(i);
+    }
+
+}
+numbersMToN(6,2);
+numbersMToN(4,1);
+
+
+function numbersMToN(m,n){
+    while(m >= n){
+        console.log(m);
+        m--;
+    }
+   
+}
+numbersMToN(6,2);
+numbersMToN(4,1);
