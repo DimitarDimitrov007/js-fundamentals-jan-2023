05cc73f92e2009b7dc1ebf7f80b0ec2ae0c3769f
Dimitar Dimitrov <117978668+DimitarDimitrov007@users.noreply.github.com>
2023-01-21 12:42:13 +0200

    Add files via upload

---
+++ Data Types and Variables Lab/Concatenate Names.js
@@ -0,0 +1,13 @@
+function concatenationOfNames(nameOne, nameTwo, delimiter) {
+    console.log(`${nameOne}${delimiter}${nameTwo}`);
+
+}
+concatenationOfNames('John', 'Smith', '->');
+concatenationOfNames('Jan',
+    'White',
+    '<->'
+);
+concatenationOfNames('Linda',
+'Terry',
+'=>'
+);
