2bcf00025521bcdcb318ae6755e82f75621d408c
Dimitar Dimitrov <117978668+DimitarDimitrov007@users.noreply.github.com>
2023-02-07 13:04:28 +0200

    Add files via upload

--- Functions - Exercise/Password Validator.js
+++ Functions - Exercise/Password Validator.js
@@ -1,16 +1,28 @@
 function passwordValidator(password) {
     let hasProperLength = passwordLengthValidation(password);
+    let hasSpecialChar = validateNonSpecialChar(password);
+    let hasTwoNumbers = containsTwoNumbers(password);
+
+    return printResult(hasProperLength,hasSpecialChar,hasTwoNumbers);
+
     function passwordLengthValidation(password) {
         return password.length >= 6 && password.length <= 10;
 
     }
-    // •	It should consist only of letters and digits
+   
     function validateNonSpecialChar(text) {
         for (let ch of text) {
             let num = ch.charCodeAt(0);
             let isLowerChar = includesLowerCase(num);
             let isUpperChar = includesUpperCase(num);
+            let isNumber = includesNumber(num);
+
+            if(!isLowerChar && !isUpperChar && !isNumber){
+                return false;
+            }
+           
         }
+        return true;
     }
 
     function includesUpperCase(num){
@@ -19,12 +31,40 @@ function passwordValidator(password) {
 
     function includesLowerCase(num) {
         return num >= 97 && num <= 122;
-
     }
-    //•	It should have at least 2 digits 
 
+    function includesNumber(num){
+        return num >= 48 && num <= 57;
+    }
+   
+    function containsTwoNumbers(password){
+        let counter = 0;
+        for(let ch of password){
+            let isNumber = includesNumber(ch.charCodeAt(0));
 
+            if(isNumber){
+                counter ++;
+            }
+        }
+        return counter >= 2;
+    }
 
+    function printResult(validLength,validSymbols,validDigits){
+        if(!validLength){
+            console.log("Password must be between 6 and 10 characters");
+        }
 
+        if(!validSymbols){
+            console.log("Password must consist only of letters and digits");
+        }
+
+        if(!validDigits){
+            console.log("Password must have at least 2 digits");
+        }
+
+        if(validLength && validSymbols && validDigits){
+            console.log("Password is valid");
+        }
+    }
 }
-passwordValidator('logIn');
+passwordValidator('Pa$s$s');
