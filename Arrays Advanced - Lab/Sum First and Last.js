d6c991c65f11d00c40ac3c925057cb89f600b728
Dimitar Dimitrov <117978668+DimitarDimitrov007@users.noreply.github.com>
2023-02-10 12:14:19 +0200

    Add files via upload

---
+++ Arrays Advanced - Lab/Sum First and Last.js
@@ -0,0 +1,7 @@
+function sumFirstAndLast(array){
+    let firstElement = Number(array[0]);
+    let lastElement = Number(array[array.length - 1]);
+    console.log(firstElement + lastElement);
+
+}
+sumFirstAndLast(['5', '10']);
