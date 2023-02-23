afb908917add673179659171b4ef78ada206ceb3
Dimitar Dimitrov <117978668+DimitarDimitrov007@users.noreply.github.com>
2023-02-23 18:45:57 +0200

    Add files via upload

---
+++ Objects and Classes/Person Info.js
@@ -0,0 +1,11 @@
+function personInfo(firstName,secondName,age){
+    let person = {
+        firstName : firstName,
+        lastName : secondName,
+        age : age,
+    }
+
+    return person;
+
+}
+console.log(personInfo("Peter","Pan","20"));
